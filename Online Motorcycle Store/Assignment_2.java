82082406921465ff78b3ea7cee4a5bc714d19695
Ahmed Helal <84647095+Ahmed-Helal-El-Sayed@users.noreply.github.com>
2021-05-23 02:38:32 +0200

    Update Assignment_2.java

--- Online Motorcycle Store/Assignment_2.java
+++ Online Motorcycle Store/Assignment_2.java
@@ -56,11 +56,17 @@ public class Assignment_2 {
 
                 switch(choice)
                 {
-                    case 1 : System.out.println("The Color is " + Halawa.getColor()+"\nThe Type is " + Halawa.getType()+"\nThe Speed is " + Halawa.getSpeed() + "\nThe Birthdate is " + Halawa.getBirthdate() + "\nThe age is "+Halawa.getAge(Halawa.getBirthdate())+"\nThe Price is "+ Halawa.getPrice() );
+                    case 1 : System.out.println("The Color is " + Halawa.getColor()+"\nThe Type is " + Halawa.getType()+"\nThe Speed is " + Halawa.getSpeed()
+                                                + "\nThe Birthdate is " + Halawa.getBirthdate() + "\nThe age is "+Halawa.getAge(Halawa.getBirthdate())+"\nThe Price is 
+                                                "+ Halawa.getPrice() );
                         break;
-                    case 2 : System.out.println("The Color is " + Hogn.getColor() +"\nThe Type is " + Hogn.getType()+"\nThe Speed is " + Hogn.getSpeed()+ "\nThe Birthdate is " + Hogn.getBirthdate() + "\nThe age is "+Hogn.getAge(Hogn.getBirthdate())+"\nThe Price is "+ Hogn.getPrice() );
+                    case 2 : System.out.println("The Color is " + Hogn.getColor() +"\nThe Type is " + Hogn.getType()+"\nThe Speed is " + Hogn.getSpeed()
+                                                + "\nThe Birthdate is " + Hogn.getBirthdate() + "\nThe age is "+Hogn.getAge(Hogn.getBirthdate())+"\nThe Price is "
+                                                + Hogn.getPrice() );
                         break; 
-                    case 3 : System.out.println("The Color is " + Dion.getColor() + "\nThe Type is " + Dion.getType()+"\nThe Speed is " + Dion.getSpeed()+"\nThe Birthdate is " + Dion.getBirthdate() + "\nThe age is "+Dion.getAge(Dion.getBirthdate())+"\nThe Price is "+ Dion.getPrice() );
+                    case 3 : System.out.println("The Color is " + Dion.getColor() + "\nThe Type is " + Dion.getType()+"\nThe Speed is " + Dion.getSpeed()
+                                                +"\nThe Birthdate is " + Dion.getBirthdate() + "\nThe age is "+Dion.getAge(Dion.getBirthdate())+"\nThe Price is "
+                                                + Dion.getPrice() );
                         break;
 
                     default :
